1f63ffdda74c7137eef081069c73c94c2e02dbfc
aliha-smchs <aliha@smgeducation.org>
2025-05-07 10:04:32 +0400

    update title and page

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter basename="/melbourne-flight-academy-fly/">
+      <BrowserRouter basename="/Falcon-Aviation-Academy/">
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -58,7 +58,7 @@ const Footer = () => {
                 <svg className="h-6 w-6 text-sky-400 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
                 </svg>
-                <span>info@melbourneflightacademy.com</span>
+                <span>info@academy.com</span>
               </li>
               <li className="flex items-center">
                 <svg className="h-6 w-6 text-sky-400 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
